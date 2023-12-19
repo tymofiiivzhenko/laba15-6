74825833f1beb2a2647d78883b96a77ba5194a3d
tymofiiivzhenko <tymofiiivzhenko@gmail.com>
2023-12-19 08:53:30 +0000

    Add project files.

---
+++ laba15-6/laba15-6.cpp
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int i, min, max, N;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &N);
+
+    int* A = (int*)malloc(N * sizeof(int));
+
+    printf("Enter the elements of the array:\n");
+    for (i = 0; i < N; ++i) {
+        printf("Element %d: ", i + 1);
+        scanf("%d", &A[i]);
+    }
+
+   min = A[0];
+   max = A[0];
+
+    for (i = 1; i < N; ++i) {
+        if (A[i] < min) {
+            min = A[i];
+        }
+        if (A[i] > max) {
+            max = A[i];
+        }
+    }
+
+    printf("Minimum element: %d\n", min);
+    printf("Maximum element: %d\n", max);
+
+    free(A);
+
+    return 420;
+}
